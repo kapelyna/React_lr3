57ac07ff712572e7d89a421bcfe58f2fc9646b69
kapelyna <katiakaplya@gmail.com>
2024-03-30 18:01:03 +0200

    CHNU_REACT_lr3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
+import { NavLink } from "react-router-dom";
 import './App.css';
+import FlatUIColors from "./components/paletteIcons/flatUIColors"
+import MaterialUIColor from "./components/paletteIcons/materialUIColor"
+import AussieUIColor from "./components/paletteIcons/aussieUIColor"
+import DutchUIColor from "./components/paletteIcons/dutchUIColor"
+import AmericanUIColor from "./components/paletteIcons/americanUIColor"
+import BritishUIColor from "./components/paletteIcons/britishUIColor"
+import SpanishUIColor from "./components/paletteIcons/spanishUIColor"
+import IndianUIColor from "./components/paletteIcons/indianUIColor"
+import FrenchUIColor from "./components/paletteIcons/frenchUIColor"
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{
+      display: 'flex', 
+      flexDirection: 'column',
+      justifyContent: 'center', 
+      alignItems: 'center', 
+      width: '100vw', 
+      height: '100vh', 
+      backgroundColor: '#3c40c6', 
+      backgroundImage: "url(https://flatuicolors.com/static/img/stars-opacity.0979c1.svg)"
+    }}>
+    
+      <div style={{
+        fontSize: "36px",
+        color: "#fff",
+        left: "2px",
+        bottom: "5px",
+        letterSpacing: "2.5px",
+        fontWeight: "900",
+        marginBottom: "20px",
+        textAlign: "left",  
+        width: "63%"       
+      }}>
+        Flat UI Colors 2
+      </div>
+    
+      <div style={{ 
+        display: 'grid', 
+        gridTemplateColumns: 'repeat(3, 1fr)', 
+        gridTemplateRows: 'repeat(3, 1fr)', 
+        gap: "20px",
+        justifyContent: 'center',  
+        alignItems: 'center',        
+      }}>
+        <NavLink to="/materialUIcolor"><MaterialUIColor/></NavLink>
+        <NavLink to="/flatUIcolor"><FlatUIColors/></NavLink>
+        <NavLink to="/dutchUIcolor"><DutchUIColor/></NavLink>
+        <NavLink to="/americanUIcolor"><AmericanUIColor/></NavLink>
+        <NavLink to="/ausieUIcolor"><AussieUIColor/></NavLink>
+        <NavLink to="/britishUIcolor"><BritishUIColor/></NavLink>
+        <NavLink to="/spanishUIcolor"><SpanishUIColor/></NavLink>
+        <NavLink to="/indianUIcolor"><IndianUIColor/></NavLink>     
+        <NavLink to="/frenchUIcolor"><FrenchUIColor/></NavLink>   
+        
+      </div>
+      
     </div>
   );
+  
+  
 }
 
 export default App;
+
--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import MaterialPage from "./components/palettePages/materialPage";
+import FlatPage from "./components/palettePages/flatPage"
+import DutchPage from "./components/palettePages/dutchPage"
+import AmericanPage from "./components/palettePages/americanPage"
+import AusiePage from "./components/palettePages/ausiePage"
+import BritishPage from "./components/palettePages/britishPage"
+import SpanishPage from "./components/palettePages/spanishPage"
+import IndianPage from "./components/palettePages/indianPage"
+import FrenchPage from "./components/palettePages/frenchPage"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<App />} />
+        <Route path="/materialUIcolor" element={<MaterialPage />} />
+        <Route path="/flatUIcolor" element={<FlatPage/>}/>
+        <Route path="/dutchUIcolor" element={<DutchPage/>}/>
+        <Route path="/americanUIcolor" element={<AmericanPage/>}/>
+        <Route path="/ausieUIcolor" element={<AusiePage/>}/>
+        <Route path="/britishUIcolor" element={<BritishPage/>}/>
+        <Route path="/spanishUIcolor" element={<SpanishPage/>}/>
+        <Route path="/indianUIcolor" element={<IndianPage/>}/>
+        <Route path="/frenchUIcolor" element={<FrenchPage/>}/>
+
+      </Routes>
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
